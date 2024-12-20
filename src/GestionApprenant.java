65f2dcd5940113550b9ae1d00e6481a69a695541
btissamtoumi <btissamtoumi5@gmail.com>
2024-12-21 00:03:39 +0100

    methode ajouter un apprenant dans la liste des apprenants

--- src/GestionApprenant.java
+++ src/GestionApprenant.java
@@ -2,7 +2,13 @@ import java.util.ArrayList;
 
 public class GestionApprenant {
     private ArrayList<Apprenant> liste_apprenant;
-
+    private int comptuerId=0;
+    public void Ajouter_Apprenant(){
+        Apprenant E1=new Apprenant();
+        E1.setId(++comptuerId);
+        liste_apprenant.add(E1);
+    }
+    
     public ArrayList<Apprenant> getListe_apprenant() {
         return liste_apprenant;
     }
