8c062731bc515d17765f746e221bd284309a5d05
btissamtoumi <btissamtoumi5@gmail.com>
2024-12-20 23:54:58 +0100

    l'ajout d'une classe qui gere les apprenants

---
+++ src/GestionApprenant.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+public class GestionApprenant {
+    private ArrayList<Apprenant> liste_apprenant;
+
+    public ArrayList<Apprenant> getListe_apprenant() {
+        return liste_apprenant;
+    }
+
+    public void setListe_apprenant(ArrayList<Apprenant> liste_apprenant) {
+        this.liste_apprenant = liste_apprenant;
+    }
+}
