9128de9f9df2526f225f4b8be01d41b24f8fa26f
btissamtoumi <btissamtoumi5@gmail.com>
2024-12-20 23:06:40 +0100

    get et set de la classe Apprenant

--- src/Apprenant.java
+++ src/Apprenant.java
@@ -4,4 +4,19 @@ public class Apprenant extends Personne {
     private Classe classe;
     private ArrayList<Double> Liste_notes;
 
+    public Classe getClasse() {
+        return classe;
+    }
+
+    public void setClasse(Classe classe) {
+        this.classe = classe;
+    }
+
+    public ArrayList<Double> getListe_notes() {
+        return Liste_notes;
+    }
+
+    public void setListe_notes(ArrayList<Double> liste_notes) {
+        Liste_notes = liste_notes;
+    }
 }
