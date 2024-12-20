6c5f36e7abadd25d60d7767b2929c30ad7f037a3
btissamtoumi <btissamtoumi5@gmail.com>
2024-12-20 23:07:49 +0100

    get et set de classe formateur

--- src/Formateur.java
+++ src/Formateur.java
@@ -4,4 +4,27 @@ public class Formateur extends  Personne {
      private  double salaire;
      private Classe classe;
 
+     public String getSpecialite() {
+          return specialite;
+     }
+
+     public void setSpecialite(String specialite) {
+          this.specialite = specialite;
+     }
+
+     public double getSalaire() {
+          return salaire;
+     }
+
+     public void setSalaire(double salaire) {
+          this.salaire = salaire;
+     }
+
+     public Classe getClasse() {
+          return classe;
+     }
+
+     public void setClasse(Classe classe) {
+          this.classe = classe;
+     }
 }
