d11534b6063cb2e43cf1752111c65baaa6c4e8d5
btissamtoumi <btissamtoumi5@gmail.com>
2024-12-21 03:20:33 +0100

    l'ajout de la classe Gestion des Formateurs pour gerer les formateurs

---
+++ src/GestionDesFormateurs.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+
+public class GestionDesFormateurs {
+    ArrayList<Formateur> liste_formateurs;
+    private int comptuerId=0;
+
+    public GestionDesFormateurs(ArrayList<Formateur> liste_formateurs) {
+        this.liste_formateurs = liste_formateurs;
+    }
+    
+}
